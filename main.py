ad04f74881d326ec76997249bc704e2175116fcb
Borja <boorjabs@gmail.com>
2022-07-26 21:32:45 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+# def is_prime(num):
+#     result = num == 2
+#
+#     if num > 1 and num % 2 != 0:
+#         result = True
+#         for item in reversed(range(2, num)):
+#             if num % item == 0:
+#                 return False
+#     return result
+
+# This is the Miller-Rabin test for primes, which works for super large n
+
+import random
+
+def even_odd(n):
+    s, d = 0, n
+    while d % 2 == 0:
+          s += 1
+          d >>= 1
+    return s, d
+
+def Miller_Rabin(a, p):
+    s, d = even_odd(p-1)
+    a = pow(a, d, p)
+    if a == 1: return True
+    for i in range(s):
+        if a == p-1: return True
+        a = pow(a, 2, p)
+    return False
+
+def is_prime(p):
+    if p == 2: return True
+    if p <= 1 or p % 2 == 0: return False
+    return all(Miller_Rabin(random.randint(2,p-1),p) for _ in range(40))
+
+
+
+print(is_prime(1))
+print(is_prime(2))
+print(is_prime(3))
+print(is_prime(4))
+print(is_prime(5))
+print(is_prime(6))
+print(is_prime(7))
+print(is_prime(8))
+print(is_prime(9))
+print(is_prime(10))
+print(is_prime(11))
+print(is_prime(12))
+print(is_prime(13))
